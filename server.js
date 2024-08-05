9964675072827983ba5cf31a4cb48363293095a7
Walii <walimalik111@gmail.com>
2024-08-05 21:33:28 +0500

    setup file

---
+++ server.js
@@ -0,0 +1,45 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/mernblog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+app.post('/blogs', async (req, res) => {
+  const newBlog = new Blog(req.body);
+  await newBlog.save();
+  res.json(newBlog);
+});
+
+app.get('/blogs', async (req, res) => {
+  const blogs = await Blog.find();
+  res.json(blogs);
+});
+
+app.put('/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(blog);
+});
+
+app.delete('/blogs/:id', async (req, res) => {
+  await Blog.findByIdAndDelete(req.params.id);
+  res.send('Blog deleted');
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});

d6543b6ed315dabfcf408e5475ddc91b6968cdc8
Walii <walimalik111@gmail.com>
2024-08-06 23:15:59 +0500

    deleteBlogs

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,61 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [blogs, setBlogs] = useState([]);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [editId, setEditId] = useState(null);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/blogs')
-      .then(response => setBlogs(response.data))
-      .catch(error => console.error('Error fetching data:', error));
+    axios
+      .get("http://localhost:5000/blogs")
+      .then((response) => setBlogs(response.data))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
-
 
   const addBlog = () => {
     if (editId) {
-      axios.put(`http://localhost:5000/blogs/${editId}`, { title, content })
-        .then(response => {
-          setBlogs(blogs.map(blog => blog._id === editId ? response.data : blog));
+      axios
+        .put(`http://localhost:5000/blogs/${editId}`, { title, content })
+        .then((response) => {
+          setBlogs(
+            blogs.map((blog) => (blog._id === editId ? response.data : blog))
+          );
           setEditId(null);
-          setTitle('');
-          setContent('');
+          setTitle("");
+          setContent("");
         })
-        .catch(error => console.error('Error updating blog:', error));
+        .catch((error) => console.error("Error updating blog:", error));
     } else {
-      axios.post('http://localhost:5000/blogs', { title, content })
-        .then(response => setBlogs([...blogs, response.data]))
-        .catch(error => console.error('Error adding blog:', error));
+      axios
+        .post("http://localhost:5000/blogs", { title, content })
+        .then((response) => setBlogs([...blogs, response.data]))
+        .catch((error) => console.error("Error adding blog:", error));
     }
   };
 
-  
   const editBlog = (id, blogTitle, blogContent) => {
     setEditId(id);
     setTitle(blogTitle);
     setContent(blogContent);
   };
 
-  const deleteBlog = id => {
-    axios.delete(`http://localhost:5000/blogs/${id}`)
-      .then(() => setBlogs(blogs.filter(blog => blog._id !== id)))
-      .catch(error => console.error('Error deleting blog:', error));
+  // updare deleteBlog function
+  const deleteBlogs = (id) => {
+    axios
+      .delete(`http://localhost:5000/blogs/${id}`)
+
+      .then(() => setBlogs(blogs.filter((blog) => blog._id !== id)))
+      .catch((error) => console.error("Error deleting blog:", error));
+  };
+
+  const deleteBlog = (id) => {
+    axios
+      .delete(`http://localhost:5000/blogs/${id}`)
+      .then(() => setBlogs(blogs.filter((blog) => blog._id !== id)))
+      .catch((error) => console.error("Error deleting blog:", error));
   };
 
   return (
@@ -51,22 +64,24 @@ function App() {
       <input
         placeholder="Title"
         value={title}
-        onChange={e => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <textarea
         placeholder="Content"
         value={content}
-        onChange={e => setContent(e.target.value)}
+        onChange={(e) => setContent(e.target.value)}
       />
-      <button onClick={addBlog}>
-        {editId ? 'Update Blog' : 'Add Blog'}
-      </button>
+      <button onClick={addBlog}>{editId ? "Update Blog" : "Add Blog"}</button>
       <ul>
-        {blogs.map(blog => (
+        {blogs.map((blog) => (
           <li key={blog._id}>
             <h2>{blog.title}</h2>
             <p>{blog.content}</p>
-            <button onClick={() => editBlog(blog._id, blog.title, blog.content)}>Edit</button>
+            <button
+              onClick={() => editBlog(blog._id, blog.title, blog.content)}
+            >
+              Edit
+            </button>
             <button onClick={() => deleteBlog(blog._id)}>Delete</button>
           </li>
         ))}

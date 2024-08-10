071bcb9febf3c138b6e41ea80148190ac5f3ed97
Wali Muhammad <77934276+walimuhammadmalik@users.noreply.github.com>
2024-08-10 23:32:17 +0500

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -30,9 +30,7 @@ const generateToken = (id) => {
 module.exports = { protect, generateToken };
 
 
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       req.user = await User.findById(decoded.id).select("-password");
-//       next();
+
 //     } catch (error) {
 //       console.error(error);
 //       res.status(401).json({ message: "Not authorized, token failed" });
